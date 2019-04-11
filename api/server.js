255348064102c0bcb29d8f544e3e8ca03c6ee6b4
Aman <amsingh714@gmail.com>
2019-04-11 16:08:10 -0400

    able to POST and DELETE characters

--- api/server.js
+++ api/server.js
@@ -16,4 +16,46 @@ server.get("/characters", (req, res) => {
     });
 });
 
+server.post("/characters", (req, res) => {
+  const character = req.body;
+
+  db("characters")
+    .insert(character)
+    .then(ids => {
+      const id = ids[0];
+      db("characters")
+        .where({ id })
+        .then(character => {
+          res.status(201).json(character);
+        });
+    })
+    .catch(error => {
+      res.status(500).json({
+        error: "There was an error while saving the character to the database."
+      });
+    });
+});
+
+server.delete("/characters/:id", (req, res) => {
+  const { id } = req.params;
+
+  db("characters")
+    .where({ id })
+    .del()
+    .then(count => {
+      if (count > 0) {
+        res.status(200).json(count);
+      } else {
+        res.status(404).json({
+          message: "The character with the specified ID does not exist."
+        });
+      }
+    })
+    .catch(error => {
+      res.status(500).json({
+        error: "The character could not be removed."
+      });
+    });
+});
+
 module.exports = server;
